d94a25605d78b567356e5b8b06346fb2d8ef46b2
obigerard18 <obigerard18@gmail.com>
2022-12-04 01:58:14 -0800

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    # Copy original list in other variable#
-    a = my_list[:]
-    if a is None:
-        return
-    if idx < 0 or idx > len(my_list) - 1:
-    return (a)
-    # Replace element#
-    a[idx] = element
-    return (a)
+    dup = my_list[:]
+    if idx >= 0 and idx < len(dup):
+        dup[idx] = element
+    return dup
