ab1f4de462e4832873012d2fecea10bfaa9aa0f3
obigerard18 <obigerard18@gmail.com>
2023-01-10 18:12:46 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""Module for saving to json"""
-import json
-import os.path
-import sys
+""" Load, add, save  """
 from sys import argv
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-json_list = []
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-if os.path.exists(filename):
-    json_list = load_from_json_file(filename)
-
-for index in argv[1:]:
-    json_list.append(index)
-
-save_to_json_file(json_list, filename)
+""" Script that adds all arguments to a Python list,
+and then save them to a file """
+try:
+    add_i = load_from_json_file('add_item.json')
+    save_to_json_file(add_i + argv[1:], 'add_item.json')
+except Exception:
+    save_to_json_file(argv[1:], 'add_item.json')
