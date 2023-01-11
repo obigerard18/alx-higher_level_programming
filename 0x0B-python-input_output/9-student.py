e243c7825c155becf220697e51ea94e2d3ab10da
obigerard18 <obigerard18@gmail.com>
2023-01-10 17:54:49 -0800

    9. Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,15 @@
-!/usr/bin/python3
-""" Student to JSON """
+#!/usr/bin/python3
+"""Module for class student"""
 
 
 class Student:
-    """ New class student """
-
+    """Class that defines a student"""
     def __init__(self, first_name, last_name, age):
+        """Constructor"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        return (self.__dict__)
+        """Retrieves dictionary"""
+        return self.__dict__.copy()
