58858e0608677b9358b9c99601cdda7579518c44
obigerard18 <obigerard18@gmail.com>
2023-01-10 17:58:38 -0800

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for class student"""
+
+
+class Student:
+    """Class that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary"""
+        if type(attrs) is list and all(type(t) is str for t in attrs):
+            return {ky: vl for ky, vl in self.__dict__.items() if ky in attrs}
+        else:
+            return self.__dict__.copy()
