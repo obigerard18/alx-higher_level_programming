f7fb635a75d9b79794cfa644d191c13228e952f6
obigerard18 <obigerard18@gmail.com>
2023-04-01 01:51:31 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""
-Python script that takes in a URL and an email, sends a POST request
-to the passed URL with the email as a parameter
-"""
+# script that fetches https://intranet.hbtn.io/status
 import urllib.request
-import urllib.parse
-import sys
+import urllib.error
 
-if __name__ == "__main__":
-
-    url = sys.argv[1]
-    new_value = {'email': sys.argv[2]}
-
-    data = urllib.parse.urlencode(new_value).encode()
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        val = response.read().decode('utf8')
-    print(val)
+try:
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        req = response.read()
+        print("Body response:\n\t- type: {}"
+              "\n\t- content: {}\n\t- utf8 content: {}"
+              .format(type(req), req, req.decode('utf-8')))
+except urllib.error.URLError as e:
+    print(e)
