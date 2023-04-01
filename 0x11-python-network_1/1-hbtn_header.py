6b7934bcb6af1202d95246f7e46e2a16add8df42
obigerard18 <obigerard18@gmail.com>
2023-04-01 02:01:44 -0700

    1-hbtn_header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,19 +1,13 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 """
-Python script that takes in a URL and an email, sends a POST request
-to the passed URL with the email as a parameter
+Python script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response.
 """
 import urllib.request
-import urllib.parse
 import sys
 
 if __name__ == "__main__":
 
-    url = sys.argv[1]
-    new_value = {'email': sys.argv[2]}
-
-    data = urllib.parse.urlencode(new_value).encode()
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        val = response.read().decode('utf8')
-    print(val)
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        value = response.info()
+    print(value['X-Request-Id'])
