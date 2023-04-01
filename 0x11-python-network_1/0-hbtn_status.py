98f40b63076639d0b9911516cd65bd419c0fffe1
obigerard18 <obigerard18@gmail.com>
2023-04-01 01:39:31 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# script that fetches https://intranet.hbtn.io/status
+import urllib.request
+import urllib.error
+
+try:
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        req = response.read()
+        print("Body response:\n\t- type: {}"
+              "\n\t- content: {}\n\t- utf8 content: {}"
+              .format(type(req), req, req.decode('utf-8')))
+except urllib.error.URLError as e:
+    print(e)
